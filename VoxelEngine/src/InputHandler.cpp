7f613544df3ca8231d72807ffbdb9a775682e696
Seung Youp Baek <bsy6766@gmail.com>
2017-08-13 03:09:24 -0500

    Updated input handler to store input states from GLFW

--- VoxelEngine/src/InputHandler.cpp
+++ VoxelEngine/src/InputHandler.cpp
@@ -1,6 +1,12 @@
 #include <InputHandler.h>
+//#include <iostream>
 
 using namespace Voxel;
+
+InputHandler::InputHandler()
+	: curX(0)
+	, curY(0)
+{}
 
 void InputHandler::glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
@@ -11,12 +17,61 @@ void InputHandler::glfwKeyCallback(GLFWwindow* window, int key, int scancode, in
 			glfwSetWindowShouldClose(window, GL_TRUE);
 		}
 	}
+
+	InputHandler::getInstance().updateKeyboard(key, action, mods);
 }
 
 void InputHandler::glfwCursorPosCallback(GLFWwindow* window, double x, double y)
 {
+	InputHandler::getInstance().updateMousePosition(x, y);
 }
 
 void InputHandler::glfwMouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
 {
+	InputHandler::getInstance().updateMouse(button, action, mods);
+}
+
+void InputHandler::updateMousePosition(double x, double y)
+{
+	curX = x;
+	curY = y;
+}
+
+void Voxel::InputHandler::updateMouse(int button, int action, int mods)
+{
+	mouseButtonMap[button] = action;
+	// Do we really need to update mods here?
+}
+
+void Voxel::InputHandler::updateKeyboard(int key, int action, int mods)
+{
+	keyMap[key] = action;
+
+	//std::cout << "Key update. key = " << key << ", action = " << action << ", mods = " << mods << std::endl;
+}
+
+void Voxel::InputHandler::getMousePosition(double & x, double & y)
+{
+	x = curX;
+	y = curY;
+}
+
+bool InputHandler::getKeyDown(int key)
+{
+	return keyMap[key] == true;
+}
+
+bool InputHandler::getKeyUp(int key)
+{
+	return keyMap[key] == false;
+}
+
+bool InputHandler::getMouseDown(int button)
+{
+	return mouseButtonMap[button] == true;
+}
+
+bool InputHandler::getMouseUp(int button)
+{
+	return mouseButtonMap[button] == false;
 }
--- VoxelEngine/src/InputHandler.h
+++ VoxelEngine/src/InputHandler.h
@@ -3,17 +3,56 @@
 
 #include <unordered_map>
 #include <GLFW\glfw3.h>
+#include <unordered_map>
 
 namespace Voxel
 {
+	/**
+	*	@class InputHandler
+	*	@brief Manages input received from GLFW. Used as singleton
+	*/
 	class InputHandler
 	{
+	protected:
+		InputHandler();
+		~InputHandler() = default;
+
+		// Mouse
+		double curX;
+		double curY;
+		std::unordered_map<int/*GLFW Mouse button*/, bool> mouseButtonMap;
+
+		// Keyboard
+		std::unordered_map<int/*GLFW Key*/, bool/*press or released*/> keyMap;
+		//std::unordered_map<int/*GLFW modifier keys*/, bool> keyModsMap;
+
+		void updateMousePosition(double x, double y);
+		void updateMouse(int button, int action, int mods);
+		void updateKeyboard(int key, int action, int mods);
 	private:
+		// Delete copy, move, assign operators
+		InputHandler(InputHandler const&) = delete;             // Copy construct
+		InputHandler(InputHandler&&) = delete;                  // Move construct
+		InputHandler& operator=(InputHandler const&) = delete;  // Copy assign
+		InputHandler& operator=(InputHandler &&) = delete;      // Move assign
 	public:
+		static InputHandler& getInstance()
+		{
+			static InputHandler instance;
+			return instance;
+		}
+
+		// GLFW callback
 		static void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
 		static void glfwCursorPosCallback(GLFWwindow* window, double x, double y);
 		static void glfwMouseButtonCallback(GLFWwindow* window, int button, int action, int mods);
-		
+
+		// InputHandler functions
+		void getMousePosition(double& x, double& y);
+		bool getKeyDown(int key);
+		bool getKeyUp(int key);
+		bool getMouseDown(int button);
+		bool getMouseUp(int button);
 	};
 }
 
