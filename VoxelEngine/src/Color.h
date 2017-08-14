77a21a90fa51fcab21cbe2c798335046a76aeb59
Seung Youp Baek <bsy6766@gmail.com>
2017-08-14 04:37:33 -0500

    added random color

--- VoxelEngine/src/Color.h
+++ VoxelEngine/src/Color.h
@@ -2,6 +2,7 @@
 #define COLOR_H
 
 #include <glm\glm.hpp>
+#include <Utility.h>
 
 namespace Voxel
 {
@@ -11,6 +12,13 @@ namespace Voxel
 		const glm::vec3 BLUE = glm::vec3(0.0f, 1.0f, 0.0f);
 		const glm::vec3 GREEN = glm::vec3(0.0f, 0.0f, 1.0f);
 		const glm::vec3 WHITE = glm::vec3(1.0f, 1.0f, 1.0f);
+
+		static glm::vec3 getRandomColor()
+		{
+			return glm::vec3(Utility::Random::randomReal(0.5f, 1.0f), 
+							Utility::Random::randomReal(0.5f, 1.0f), 
+							Utility::Random::randomReal(0.5f, 1.0f));
+		}
 	}
 }
 
