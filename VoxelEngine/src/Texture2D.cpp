5b949a3f6a8d4e9fb41f32e8f6d8a25121b0329b
Seung Youp Baek <bsy6766@gmail.com>
2017-08-18 18:32:25 -0500

    implementing texture 2d class

--- VoxelEngine/src/Texture2D.cpp
+++ VoxelEngine/src/Texture2D.cpp
@@ -1,11 +1,133 @@
 #include "Texture2D.h"
+#include <stb_image.h>
 
 using namespace Voxel;
 
+const std::string Texture2D::DEFAULT_TEXTURE_PATH = "textures/";
+
 Texture2D::Texture2D()
+	: textureObject(0)
+	, textureLocation(0)
+	, width(0)
+	, height(0)
+	, channel(0)
 {
 }
 
 Texture2D::~Texture2D()
 {
+	glDeleteTextures(1, &textureObject);
+}
+
+Texture2D * Voxel::Texture2D::create(const std::string & textureFilePath, GLenum textureTarget)
+{
+	auto newTexture = new Texture2D();
+	if (newTexture->init(textureFilePath, textureTarget))
+	{
+		return newTexture;
+	}
+	else
+	{
+		delete newTexture;
+		return nullptr;
+	}
+}
+
+bool Voxel::Texture2D::init(const std::string & textureFilePath, GLenum textureTarget)
+{
+	unsigned char* data = loadImage(textureFilePath, this->width, this->height, this->channel);
+
+	if (width == 0 || height == 0 || channel == 0)
+	{
+		throw std::runtime_error("Bad texture file");
+	}
+
+	this->textureTarget = textureTarget;
+
+	if (data)
+	{
+		generate2DTexture(width, height, channel, data);
+
+		stbi_image_free(data);
+	}
+	else
+	{
+		return false;
+	}
+}
+
+unsigned char * Voxel::Texture2D::loadImage(const std::string& textureFilePath, int & width, int & height, int & channel)
+{
+	auto filePath = Texture2D::DEFAULT_TEXTURE_PATH + textureFilePath;
+
+	FILE* file = nullptr;
+	auto err = fopen_s(&file, filePath.c_str(), "rb");
+	if (err != 0)
+	{
+		throw std::runtime_error("Bad texture filepath");
+	}
+
+	/*
+		stbi_load_from_file
+		stb_image reads from top-left most in the image.
+		pixel data has y scanelines of x pixels
+		But glTexImage2D reads pixal data from bottom left. See initTexture()
+	*/
+	unsigned char* data = stbi_load_from_file(file, &width, &height, &channel, 0);
+	// So.. we filp image 
+	flipImage(data);
+
+	fclose(file);
+
+	return data;
+}
+
+void Voxel::Texture2D::flipImage(unsigned char * data)
+{
+	unsigned long rowSize = channel * width;
+	unsigned char* rowBuffer = new unsigned char[rowSize];
+	unsigned halfRows = height / 2;
+
+	for (unsigned rowIdx = 0; rowIdx < halfRows; ++rowIdx) {
+		unsigned char* row = data + ((rowIdx * width + 0) * channel);
+		unsigned char* oppositeRow = data + (((height - rowIdx - 1) * width + 0) * channel);
+
+		std::memcpy(rowBuffer, row, rowSize);
+		std::memcpy(row, oppositeRow, rowSize);
+		std::memcpy(oppositeRow, rowBuffer, rowSize);
+	}
+
+	delete[] rowBuffer;
+}
+
+void Voxel::Texture2D::generate2DTexture(const int width, const int height, const int channel, unsigned char * data)
+{
+	glGenTextures(1, &textureObject);
+	glBindTexture(textureTarget, textureObject);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+	switch (static_cast<Channel>(channel))
+	{
+	case Channel::GRAYSCALE:
+		glTexImage2D(textureTarget, 0, GL_RGBA8, width, height, 0, GL_LUMINANCE, GL_UNSIGNED_BYTE, data);
+		break;
+	case Channel::GRAYSCALE_ALPHA:
+		glTexImage2D(textureTarget, 0, GL_RGBA8, width, height, 0, GL_LUMINANCE_ALPHA, GL_UNSIGNED_BYTE, data);
+		break;
+	case Channel::RGB:
+		// JPEG
+		glTexImage2D(textureTarget, 0, GL_RGBA8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		break;
+	case Channel::RGBA:
+		// PNG
+		glTexImage2D(textureTarget, 0, GL_RGBA8, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		break;
+	case Channel::NONE:
+	default:
+		break;
+	}
 }
--- VoxelEngine/src/Texture2D.h
+++ VoxelEngine/src/Texture2D.h
@@ -3,20 +3,49 @@
 
 #include <GL\glew.h>
 #include <glm\glm.hpp>
+#include <string>
 
 namespace Voxel
 {
 	/**
-	*	@class 
+	*	@class Texture2D
+	*	@brief Contains OpenGL Texture data. Only supports PNG
 	*/
 	class Texture2D
 	{
+	public:
+		enum class Channel : int
+		{
+			NONE = 0,
+			GRAYSCALE = 1,
+			GRAYSCALE_ALPHA = 2,
+			RGB = 3,
+			RGBA = 4
+		};
+
+		const static std::string DEFAULT_TEXTURE_PATH;
 	private:
 		Texture2D();
+
+		GLuint textureObject;
+		GLenum textureTarget;
+		GLint textureLocation;
+
+		int width;
+		int height;
+
+		int channel;
+
+		unsigned char* loadImage(const std::string& textureFilePath, int& width, int& height, int& channel);
+		void flipImage(unsigned char* data);
+
+		void generate2DTexture(const int width, const int height, const int channel, unsigned char* data);
+
+		bool init(const std::string& textureFilePath, GLenum textureTarget);
 	public:
 		~Texture2D();
 
-		static Texture2D* create();
+		static Texture2D* create(const std::string& textureFilePath, GLenum textureTarget);
 
 		void bind(GLenum textureUnit);
 	};
