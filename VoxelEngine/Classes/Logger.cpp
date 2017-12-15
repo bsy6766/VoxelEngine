f2ebd568e4301ae3c4bd363dfcd68d9f0bd963f9
Seung Youp Baek <bsy6766@gmail.com>
2017-12-15 11:29:20 -0600

    Adding logger

---
+++ VoxelEngine/Classes/Logger.cpp
@@ -0,0 +1,67 @@
+#include "Logger.h"
+
+// spdlog
+#include <spdlog\spdlog.h>
+
+// voxel
+#include "FileSystem.h"
+
+Voxel::Logger::Logger()
+#if V_DEBUG && V_DEBUG_LOG_CONSOLE
+	: console(spdlog::stdout_color_mt("console"))
+#endif
+{
+#if V_DEBUG && V_DEBUG_LOG_CONSOLE
+	console->set_pattern("[%P] [%l] [%c] %v");
+#endif
+
+	auto fs = &Voxel::FileSystem::getInstance();
+
+	auto logFilePath = Voxel::FileSystem::getInstance().getWorkingDirectory() + "/log.txt";
+
+	if (fs->doesPathExists(logFilePath))
+	{
+		fs->deleteFile(logFilePath);
+	}
+	
+	logger = spdlog::basic_logger_mt("Log", logFilePath);
+
+	logger->set_pattern("[%P] [%l] [%c] %v");
+}
+
+void Voxel::Logger::info(const std::string & log)
+{
+#if V_DEBUG && V_DEBUG_LOG_CONSOLE
+	console->info(log);
+#endif
+
+	logger->info(log);
+}
+
+void Voxel::Logger::warn(const std::string & log)
+{
+#if V_DEBUG && V_DEBUG_LOG_CONSOLE
+	console->warn(log);
+#endif
+
+	logger->warn(log);
+}
+
+void Voxel::Logger::error(const std::string & log)
+{
+#if V_DEBUG && V_DEBUG_LOG_CONSOLE
+	console->error(log);
+#endif
+
+	logger->error(log);
+}
+
+void Voxel::Logger::flush()
+{
+	logger->flush();
+}
+
+void Voxel::Logger::consoleInfo(const std::string & log)
+{
+	console->info(log);
+}
---
+++ VoxelEngine/Classes/Logger.h
@@ -0,0 +1,66 @@
+#ifndef LOGGER_H
+#define LOGGER_H
+// cpp
+#include <memory>
+#include <string>
+
+// voxel
+#include "Config.h"
+
+// foward delcaration
+namespace spdlog
+{
+	class logger;
+}
+
+namespace Voxel
+{
+
+	class Logger
+	{
+	private:
+		// Constructor
+		Logger();
+
+		// Destructor
+		~Logger() = default;
+
+		// basic logger
+		std::shared_ptr<spdlog::logger> logger;
+
+#if V_DEBUG && V_DEBUG_LOG_CONSOLE
+		// output console only when debug. Console will not appera on release build
+		std::shared_ptr<spdlog::logger> console;
+#endif
+
+	public:
+		static Logger& getInstance()
+		{
+			static Logger instance;
+			return instance;
+		}
+
+		Logger(Logger const&) = delete;             // Copy construct
+		Logger(Logger&&) = delete;                  // Move construct
+		Logger& operator=(Logger const&) = delete;  // Copy assign
+		Logger& operator=(Logger &&) = delete;      // Move assign
+		
+		// log info
+		void info(const std::string& log);
+
+		// log warn
+		void warn(const std::string& log);
+
+		// log error
+		void error(const std::string& log);
+
+		// flush
+		void flush();
+
+#if V_DEBUG && V_DEBUG_LOG_CONSOLE
+		void consoleInfo(const std::string& log);
+#endif
+	};
+}
+
+#endif
