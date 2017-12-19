0bf058d7a39ed375745b402552ad36dbb72e0466
Seung Youp Baek <bsy6766@gmail.com>
2017-12-18 23:00:38 -0600

    Adding pch

---
+++ VoxelEngine/Classes/PreCompiled.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#ifndef PRE_COMPILED_H
+#define PRE_COMPILED_H
+
+// cpp
+#include <iostream>
+#include <string>
+
+// gl
+#include <GL\glew.h>
+
+// glfw
+#include <GLFW\glfw3.h>
+
+// glm
+#include <glm\glm.hpp>
+#include <glm\gtx\transform.hpp>
+
+// json
+#include <json.hpp>
+
+// stb_image
+#include <stb_image.h>
+
+// voxel
+#include "Utility.h"
+#include "Logger.h"
+#include "ErrorCode.h"
+#include "Color.h"
+#include "Quad.h"
+#include "LocalizationTags.h"
+
+#endif
