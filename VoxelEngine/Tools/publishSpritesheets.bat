1060c16dc2ea341b40209994785d9a5da4ea2031
unknown <bsy6766@gmail.com>
2018-03-15 23:56:52 -0500

    Added quotes around paths

--- VoxelEngine/Tools/publishSpritesheets.bat
+++ VoxelEngine/Tools/publishSpritesheets.bat
@@ -1,9 +1,9 @@
-"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" %cd%/SpriteSheets/DebugSpriteSheet.tps
-"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" %cd%/SpriteSheets/CursorSpriteSheet.tps
-"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" %cd%/SpriteSheets/EditorUISpriteSheet.tps
-"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" %cd%/SpriteSheets/EnvironmentSpriteSheet.tps
-"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" %cd%/SpriteSheets/GlobalSpriteSheet.tps
-"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" %cd%/SpriteSheets/MenuSceneUISpriteSheet.tps
-"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" %cd%/SpriteSheets/UISpriteSheet.tps
-"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" %cd%/SpriteSheets/ParticleSpriteSheet.tps
-"C:\Python27\python.exe" %cd%/Tools/jsonTrim.py
+"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" "%cd%/SpriteSheets/DebugSpriteSheet.tps"
+"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" "%cd%/SpriteSheets/CursorSpriteSheet.tps"
+"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" "%cd%/SpriteSheets/EditorUISpriteSheet.tps"
+"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" "%cd%/SpriteSheets/EnvironmentSpriteSheet.tps"
+"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" "%cd%/SpriteSheets/GlobalSpriteSheet.tps"
+"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" "%cd%/SpriteSheets/MenuSceneUISpriteSheet.tps"
+"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" "%cd%/SpriteSheets/UISpriteSheet.tps"
+"C:\Program Files\CodeAndWeb\TexturePacker\bin\TexturePacker.exe" "%cd%/SpriteSheets/ParticleSpriteSheet.tps"
+"C:\Python27\python.exe" "%cd%/Tools/jsonTrim.py"
